e3373739863770339a835c572aa04419d688c570
hossam <pos_r_d@surepay.sa>
2022-09-11 15:15:23 +0200

    - TestAuthRepositoryImpl.kt code enhancement

--- auth/auth_data/src/main/java/com/surepay/auth_data/repository/TestAuthRepositoryImpl.kt
+++ auth/auth_data/src/main/java/com/surepay/auth_data/repository/TestAuthRepositoryImpl.kt
@@ -5,16 +5,14 @@ import com.surepay.auth_domain.model.Login
 import com.surepay.auth_domain.repositpry.AuthRepository
 import com.surepay.core.util.Resource
 import kotlinx.coroutines.delay
-import java.net.UnknownHostException
 
 class TestAuthRepositoryImpl (
     private val authApi: AuthApi
 ): AuthRepository {
     override suspend fun login(email: String, password: String): Resource<Login> {
         delay(1000*2)
-        val data = Resource.Success(Login(true,1,"",1))
-//        throw UnknownHostException("Fake No internet")
-        return data;
+        return Resource.Success(Login(true,1,"",1))
+
         
     }
 }
